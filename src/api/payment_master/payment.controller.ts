bee76b3bb1ec3ddea0dadb5ac877f474d4d3d183
kaushik <kaushikgangani8894@gmail.com>
2022-07-21 15:28:24 +0530

    build:payment-transfer

--- src/api/payment_master/payment.controller.ts
+++ src/api/payment_master/payment.controller.ts
@@ -12,6 +12,7 @@ import { Auth } from "../../middleware/auth";
 import UserService from "../user/user.service";
 import { STRIPE_SECRET_KEY } from "../../config";
 import { payoutDTO } from "./payment.validator";
+import BankService from "../bankMaster/bank.service";
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
 const moment = require("moment");
 const time = moment.utc().valueOf();
@@ -20,6 +21,7 @@ const time = moment.utc().valueOf();
 @UseBefore(Auth)
 export default class PaymentController {
   private userService: UserService = new UserService();
+  private bankService: BankService = new BankService();
 
   @Get("/verify-link", { transformResponse: true })
   async register(@Req() request: any, @Res() response: any) {
@@ -91,7 +93,7 @@ export default class PaymentController {
     try {
       const { amount, bank_account } = body;
 
-      const userData: any = await this.userService.findOne({
+      const userData: any = await this.bankService.findOne({
         _id: request.data.id,
       });
 
@@ -106,7 +108,8 @@ export default class PaymentController {
 
       if (!bank_account) {
         const resultBankAccounts: any = await this.userService.findOne({
-          _id: request.data.id,
+          sellerId: request.data.id,
+          is_deleted: false,
         });
       }
 
